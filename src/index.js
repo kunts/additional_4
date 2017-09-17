b3499c978ba9a11120f00e56d5d5d2ee3385512a
Alena.Kuntsevich <ekuntsevich@yandex.ru>
2017-09-17 22:14:39 +0300

    feature: multiply numbers with result length < 15 symbols, converting numbs with longer results to array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var res = "";
+if(first.length + second.length <14){
+  res += (first*second);
+}else{
+var A = toArr(first);
+var B = toArr(second);
+console.log ("A " + A);
+console.log("B " +B);
+}
+
+return res;
+}
+
+
+
+function toArr(str){
+  var arr = [];
+  if (str.length > 3){
+  for(var i = str.length -1; i >= 0 ; i -= 3){
+    arr.push(+(str.substr(i,3)));
+  }
+  if(str.length % 3!=0 ){
+        arr.push(+str.substr(0,(str.length % 3)));
+      }
+} else {
+  arr.push (str.slice(0));
+}
+return arr;
 }
